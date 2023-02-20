65a740b2746bf78aa17237be6b06656743cc0848
MoeAlm <almazwzy8@gmail.com>
2023-02-20 22:11:16 +0200

    Initial commit

--- lib/view/home/search_layout.dart
+++ lib/view/home/search_layout.dart
@@ -8,26 +8,29 @@ class SearchLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Center(
-        child: TextField(
-          autofocus: true,
-          decoration: InputDecoration(
-              labelText: 'Search',
-              prefixIcon: Icon(
-                Icons.search,
-                color: Colors.black54,
-                size: height * 0.04,
-              ),
-              suffixIcon: Icon(Icons.filter_alt_outlined,
-                  color: Colors.black54, size: height * 0.04),
-              filled: true,
-              fillColor: Colors.grey[300],
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-                borderSide: BorderSide.none,
-              )),
-        ).py12(),
-      ),
+      body: Column(
+        children: [
+          SizedBox(height: height * 0.1,),
+          TextField(
+            autofocus: true,
+            decoration: InputDecoration(
+                labelText: 'Search',
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.black54,
+                  size: height * 0.04,
+                ),
+                suffixIcon: Icon(Icons.filter_alt_outlined,
+                    color: Colors.black54, size: height * 0.04),
+                filled: true,
+                fillColor: Colors.grey[300],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none,
+                )),
+          ),
+        ],
+      ).py12().px16(),
     );
   }
 }
