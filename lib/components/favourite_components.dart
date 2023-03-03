ca4c8864df1604aaae63b6d05fdfce29604698d1
MoeAlm <almazwzy8@gmail.com>
2023-03-03 19:30:25 +0200

    Initial commit

---
+++ lib/components/favourite_components.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:food_app_api/components/text_components.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import '../core/model/food_model.dart';
+
+Widget favouriteItem(double height, double width,
+    {required Food model, required Function() onPressed}) {
+  return Card(
+    color: Colors.white,
+    child: Container(
+      height: height * 0.2,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            model.img,
+            height: height * 0.3,
+            width: width * 0.3,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              buildText(width,
+                  text: model.title,
+                  size: 0.065,
+                  color: Colors.black,
+                  weight: FontWeight.w600),
+              buildText(width,
+                  text: model.subTitle,
+                  size: 0.05,
+                  color: Colors.black45,
+                  weight: FontWeight.w600),
+              buildText(
+                width,
+                text: '\$${model.price}',
+                size: 0.08,
+                color: Colors.black45,
+                weight: FontWeight.w600,
+              ),
+            ],
+          ).py12(),
+          IconButton(
+            onPressed: onPressed,
+            icon: Icon(Icons.delete),
+          )
+        ],
+      ),
+    ),
+  );
+}
--- lib/view/fav_food_screen.dart
+++ lib/view/fav_food_screen.dart
@@ -5,7 +5,8 @@ import 'package:food_app_api/core/cubit/cubit.dart';
 import 'package:food_app_api/core/cubit/states.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-import '../core/model/food_model.dart';
+import '../components/favourite_components.dart';
+import 'detail_screen.dart';
 
 class FavouriteFood extends StatelessWidget {
   const FavouriteFood({Key? key}) : super(key: key);
@@ -36,64 +37,26 @@ class FavouriteFood extends StatelessWidget {
           body: ListView.builder(
             itemCount: cubit.likedIndex.length,
             itemBuilder: (BuildContext context, int index) {
-              return favouriteItem(height, width,
-                  model: cubit.likedIndex[index], onPressed: () {
-                cubit.removeItem(index);
-              }).pOnly(top: 12).px12();
+              return InkWell(
+                onTap: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailsScreen(
+                        model: cubit.food[index],
+                      ),
+                    ),
+                  );
+                },
+                child: favouriteItem(height, width,
+                    model: cubit.likedIndex[index], onPressed: () {
+                  cubit.removeItem(index);
+                }).pOnly(top: 12).px12(),
+              );
             },
           ),
         );
       },
-    );
-  }
-
-  Widget favouriteItem(double height, double width,
-      {required Food model, required Function() onPressed}) {
-    return Card(
-      color: Colors.white,
-      child: Container(
-        height: height * 0.2,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Image.asset(
-              model.img,
-              height: height * 0.3,
-              width: width * 0.3,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                buildText(width,
-                    text: model.title,
-                    size: 0.065,
-                    color: Colors.black,
-                    weight: FontWeight.w600),
-                buildText(width,
-                    text: model.subTitle,
-                    size: 0.05,
-                    color: Colors.black45,
-                    weight: FontWeight.w600),
-                buildText(
-                  width,
-                  text: '\$${model.price}',
-                  size: 0.08,
-                  color: Colors.black45,
-                  weight: FontWeight.w600,
-                ),
-              ],
-            ).py12(),
-            IconButton(
-              onPressed: onPressed,
-              icon: Icon(Icons.delete),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
